29f3b4d0b87f4ff338ca7d1be54cddda5adb7d77
David Rillh <david.rillh@soprasteria.com>
2021-10-13 00:18:35 +0200

    Here are som modifications I made and II created a README and a Neural Network !

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-import random
-a = random.randrange(1, 45)
-print(a)
+import numpy as np
+import math as m
+
+
+class Neuron:
+    def __init__(self, input_size, activation):
+        self.weights = np.round(np.random.rand(1, input_size + 1))
+        self.activation = activation
+
+
+def sigmoid(x):
+    sig = 1 / (1 + m.exp(-x))
+    return sig
+
+
+neuron = Neuron(24, sigmoid)
+print(neuron.activation(0.5))
