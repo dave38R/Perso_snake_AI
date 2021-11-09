9bc09a82abda1d9f46d39216e008f7bce110ca14
dave <david.rillh@hotmail.com>
2021-11-09 21:25:59 +0100

    All the functions to get the info were tested and they yield pretty good results.

--- test.py
+++ test.py
@@ -12,3 +12,39 @@ input_vector = np.round(np.random.uniform(-1, 1, 24), 2)
 result = Net.forward_propagation(input_vector)
 
 decision = np.argmax(result)
+
+print(result)
+print("decision :", decision)
+
+def get_distance(body_list, bit):
+    head = body_list[0]
+    checkpoints = []
+
+    for pos in body_list:
+
+        if head[bit] == pos[bit]:
+            checkpoints.append(head[1 - bit] - pos[1 - bit])  # We add all the distances that we get, pos or neg
+
+    pos_checkpoints = [distance for distance in checkpoints if distance > 0]  # Neg part ==> left or down parts
+    if len(pos_checkpoints) == 0:
+        pos_checkpoints.append(0)  # 0 means that there are no snake parts in that direction
+
+    neg_checkpoints = [distance for distance in checkpoints if distance < 0]  # Pos part ==> right or up parts
+    if len(neg_checkpoints) == 0:
+        neg_checkpoints.append(0)  # 0 means that there are no snake parts in that direction
+
+    distance_to_body = [max(neg_checkpoints), min(pos_checkpoints)]  # We have the pos and neg closest to the snake
+
+    return distance_to_body
+
+
+def get_info_body(body_list):
+    distance_to_body_x = get_distance(body_list, 0)
+    distance_to_body_y = get_distance(body_list, 1)
+
+    return distance_to_body_x, distance_to_body_y
+
+
+body = [[50, 50], [50, 100], [50, 25]]
+print(get_info_body(body))
+
