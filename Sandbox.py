a7162fc9cfabb0529b4044eac7439b84e23792c9
dave <david.rillh@hotmail.com>
2021-11-16 19:12:31 +0100

    Just a program to test things.

--- Sandbox.py
+++ Sandbox.py
@@ -1,11 +1,1 @@
-from Neural_Network import Layer, Activation
-import numpy as np
-
-# We will create an input layer totally random, it's just to see if forward propagation works
-input_vector = np.round(0.1 * np.random.uniform(-1, 1, 3), 2)
-
-# Try to work out why it doesn't work
-layer = Layer(4, Activation.relu)
-o = layer.forward_layer(input_vector)
-
 
