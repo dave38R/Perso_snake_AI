959984fd593d869ac682685150b63df44552420e
david <david.rillh1@gmail.com>
2021-11-30 19:56:37 +0100

    information functions seem to work fine

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from Neural_Network import Layer
 from Neural_Network import NeuralNetwork
 from Neural_Network import Activation
 import numpy as np
+from Snakes.Information_Functions import get_info
+
 #  Creating our neural network, we called it Net. Now we can do whatever we want with it !
 Net = NeuralNetwork(24, 4, Layer(18, Activation.relu))
 Net.add_layer(18, Activation.sigmoid)
@@ -13,38 +15,13 @@ result = Net.forward_propagation(input_vector)
 
 decision = np.argmax(result)
 
-print(result)
-print("decision :", decision)
-
-def get_distance(body_list, bit):
-    head = body_list[0]
-    checkpoints = []
-
-    for pos in body_list:
-
-        if head[bit] == pos[bit]:
-            checkpoints.append(head[1 - bit] - pos[1 - bit])  # We add all the distances that we get, pos or neg
-
-    pos_checkpoints = [distance for distance in checkpoints if distance > 0]  # Neg part ==> left or down parts
-    if len(pos_checkpoints) == 0:
-        pos_checkpoints.append(0)  # 0 means that there are no snake parts in that direction
-
-    neg_checkpoints = [distance for distance in checkpoints if distance < 0]  # Pos part ==> right or up parts
-    if len(neg_checkpoints) == 0:
-        neg_checkpoints.append(0)  # 0 means that there are no snake parts in that direction
-
-    distance_to_body = [max(neg_checkpoints), min(pos_checkpoints)]  # We have the pos and neg closest to the snake
-
-    return distance_to_body
+# print(result)
+# print("decision :", decision)
 
 
-def get_info_body(body_list):
-    distance_to_body_x = get_distance(body_list, 0)
-    distance_to_body_y = get_distance(body_list, 1)
-
-    return distance_to_body_x, distance_to_body_y
+body = [[130, 30], [140, 30], [150, 30], [150, 40], [150, 50], [150, 60], [150, 70], [140, 70], [130, 70], [120, 70], [110, 70], [100, 70], [90, 70], [80, 70]]
+fruit = [10, 90]
+print(get_info(body, fruit))
 
 
-body = [[50, 50], [50, 100], [50, 25]]
-print(get_info_body(body))
 
