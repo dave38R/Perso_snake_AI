6a666b41a2e9f31787528da43c1db831448ceaad
david <david.rillh1@gmail.com>
2021-11-30 19:55:12 +0100

    AI plays snake

--- Snakes/AI_Plays_Snake.py
+++ Snakes/AI_Plays_Snake.py
@@ -1,51 +1,20 @@
 # This code is from this website : https://www.geeksforgeeks.org/snake-game-in-python-using-pygame-module/
 
 import pygame
-import random
 from Neural_Network import Net
 import numpy as np
+from Game_Parameters import *
+from Information_Functions import *
+
 
 pygame.font.init()
 pygame.init()
-
-# Window size
-window_x = 450
-window_y = 300
-# defining colors
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
 
 # Initialise game window
 pygame.display.set_caption("David's Snake Game !")
 game_window = pygame.display.set_mode((window_x, window_y))
 
-# FPS (frames per second) controller
-fps = pygame.time.Clock()
 
-# defining snake default position
-snake_position = [100, 50]
-snake_speed = 15
-# defining first 4 blocks of snake
-# body
-snake_body = [[100, 50],
-              [90, 50],
-              [80, 50],
-              [70, 50]]
-# fruit position
-fruit_position = [random.randrange(1, (window_x // 10)) * 10,
-                  random.randrange(1, (window_y // 10)) * 10]
-fruit_spawn = True
-
-# setting default snake direction
-# towards right
-direction = 'RIGHT'
-change_to = direction
-
-# initial score
-score = 0
 
 
 def spawn_a_fruit(snake_body):  # We can't have a fruit spawn ON the snake.
@@ -102,56 +71,6 @@ def game_over(gameOver, score):
             if event.type == pygame.QUIT:
                 quit()
 
-
-# Here are the functions to get the information we want to feed our algorithm
-def get_distance(body_list, bit):
-    head = body_list[0]
-    checkpoints = []
-
-    for pos in body_list:
-
-        if head[bit] == pos[bit]:
-            checkpoints.append(pos[1 - bit] - head[1 - bit])  # We add all the distances that we get, pos or neg
-
-    pos_checkpoints = [distance for distance in checkpoints if distance > 0]  # Neg part ==> left or down parts
-    if len(pos_checkpoints) == 0:
-        pos_checkpoints.append(0)  # 0 means that there are no snake parts in that direction
-
-    neg_checkpoints = [distance for distance in checkpoints if distance < 0]  # Pos part ==> right or up parts
-    if len(neg_checkpoints) == 0:
-        neg_checkpoints.append(0)  # 0 means that there are no snake parts in that direction
-
-    distance_to_body = [max(neg_checkpoints), min(pos_checkpoints)]  # We have the pos and neg closest to the snake
-
-    return distance_to_body
-
-
-def get_info_body(body_list):
-    distance_to_body_x = get_distance(body_list, 0)
-    distance_to_body_y = get_distance(body_list, 1)
-
-    return distance_to_body_x, distance_to_body_y
-
-
-def get_info(body_list=None, fruit_position=None):
-    if fruit_position is None:
-        fruit_position = fruit_list
-    if body_list is None:
-        body_list = snake_body
-    head = body_list[0]
-    info_vector = []
-    info_vector.append(head[0])  # Distance to left wall
-    info_vector.append(window_x - head[0])  # Distance to right wall
-    info_vector.append(head[1])  # Distance to bottom wall
-    info_vector.append(window_y - head[1])  # Distance to top wall
-    info_vector.append(fruit_position[0] - head[0])  # Distance to fruit along the x axis
-    info_vector.append(fruit_position[1] - head[1])  # Distance to fruit along the y axis
-    info_vector.append(get_info_body(body_list)[0][0])  # Distance to body on the bottom
-    info_vector.append(get_info_body(body_list)[0][1])  # Distance to body on the top
-    info_vector.append(get_info_body(body_list)[1][0])  # Distance to body on the left
-    info_vector.append(get_info_body(body_list)[1][1])  # Distance to body on the right
-
-    return info_vector
 
 
 # Main Function
@@ -244,7 +163,5 @@ def play_game(change_to='RIGHT', direction='RIGHT', fruit_list=fruit_position, f
 
     return score
 
-b = 3
-a = play_game()
-print(a, b)
+print(play_game())
 
